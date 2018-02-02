93d470429106342ae595cf48eed4e9a92bf8f016
nicoh <nicolas.hersant@gmail.com>
2018-02-02 15:11:32 +0100

    modif dbconfig

--- db-config.js
+++ db-config.js
@@ -2,6 +2,7 @@ const mysql = require('mysql');
 
 const db  = mysql.createPool({
   host     : '10.1.4.28',
+  port      : '3306',
   user     : 'jenkins_user',
   password : 'VlR1d1oiqrccMxid',
   database : 'api_jenkins'
